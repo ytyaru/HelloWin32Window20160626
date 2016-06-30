e860192fb86e039c2d7aea7a466a266365f7ae7a
ytyaru <ytyaru@outlook.jp>
2016-06-30 15:41:13 +0900

    initial commit

---
+++ HelloWin32Window20160626/Program.cpp
@@ -0,0 +1,57 @@
+#include <windows.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+TCHAR szClassName[] = _T("HelloWin32Window20160626");
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst,
+                   LPSTR lpszCmdLine, int nCmdShow)
+{
+    HWND hWnd;
+    MSG msg;
+    WNDCLASS myProg;
+    if (!hPreInst) {
+        myProg.style            = CS_HREDRAW | CS_VREDRAW;
+        myProg.lpfnWndProc      = WndProc;
+        myProg.cbClsExtra       = 0;
+        myProg.cbWndExtra       = 0;
+        myProg.hInstance        = hInstance;
+        myProg.hIcon            = NULL;
+        myProg.hCursor          = LoadCursor(NULL, IDC_ARROW);
+		myProg.hbrBackground    = (HBRUSH)(COLOR_WINDOW+1);
+        myProg.lpszMenuName     = NULL;
+        myProg.lpszClassName    = szClassName;
+        if (!RegisterClass(&myProg))
+            return FALSE;
+    }
+    hWnd = CreateWindow(szClassName,	// class名
+		_T("Hello Window !! こんにちは窓！"),			// タイトル
+        WS_OVERLAPPEDWINDOW,		// Style
+        CW_USEDEFAULT,				// X
+        CW_USEDEFAULT,				// Y
+        CW_USEDEFAULT,				// Width
+        CW_USEDEFAULT,				// Height
+        NULL,						// 親ウィンドウまたはオーナーウィンドウのハンドル
+        NULL,						// メニューハンドルまたは子ウィンドウ ID
+        hInstance,					// アプリケーションインスタンスのハンドル
+        NULL);						// ウィンドウ作成データ
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return (msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return(DefWindowProc(hWnd, msg, wParam, lParam));
+    }
+    return (0L);
+}
---
+++ ReadMe.md
@@ -0,0 +1,22 @@
+# このソフトウェアについて #
+
+HelloWin32Window20160626は私個人が学習目的で作成したソフトウェアです。
+
+WindowsAPIを使ってWindowを表示しました。
+以後、これを使いまわしてWindowプログラミングできそう。
+
+# 開発環境 #
+
+* Windows XP SP3
+* C++/Win32
+* Visual C++ 2010 Express
+
+# イメージ #
+
+![HelloWin32Window](http://cdn-ak.f.st-hatena.com/images/fotolife/y/ytyaru/20160630/20160630122122.png)
+
+# ライセンス #
+
+このソフトウェアはCC0ライセンスです。
+
+[![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png "CC0")](http://creativecommons.org/publicdomain/zero/1.0/deed.ja)
